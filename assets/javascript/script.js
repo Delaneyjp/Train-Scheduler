2267b8dc85ca021d04d036639918794ced239917
JPDent <jdelaney.ent@outlook.com>
2019-04-30 12:55:00 -0700

    failed fixing bug

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,6 +31,11 @@ $(document).ready(function () {
         console.log(arrivalTime);
         console.log(frequency);
 
+        $("#train-column").text(snapshot.val().trainName);
+        $("#train-destination").text(snapshot.val().destination);
+        $("#train-arrival").text(snapshot.val().arrivalTime);
+        $("#train-freq").text(snapshot.val().frequency);
+
         // database.ref().push({
         //     trainName: trainName,
         //     destination: destination,
@@ -50,20 +55,22 @@ $(document).ready(function () {
         // database.push
         database.ref().push(newTrain);
 
+
+
     });
 
 
-    // // When a firebase event is detected, add a row for the new train
+    // // // When a firebase event is detected, add a row for the new train
     database.ref().on("child_added", function (childSnapshot) {
 
-        //     // Store everything into a variable.
-        var trainName = childSnapshot.val().trainName;
-        var destination = childSnapshot.val().destination;
-        var arrivalTime = childSnapshot.val().arrivalTime;
-        var frequency = childSnapshot.val().frequency;
+        //     //     // Store everything into a variable.
+        // var trainName = childSnapshot.val().trainName;
+        // var destination = childSnapshot.val().destination;
+        // var arrivalTime = childSnapshot.val().arrivalTime;
+        // var frequency = childSnapshot.val().frequency;
 
 
-        // Calculate mins to next train
+        //     // Calculate mins to next train
         var currentTime = moment();
         arrivalTime = moment(arrivalTime, "HH mm");
 
@@ -78,29 +85,29 @@ $(document).ready(function () {
             var arrivalTime = moment().add(nextTrain, "minutes").format("HH:mm");
         }
 
-        // Add each train's data into the table 
+        //     // Add each train's data into the table 
         $("#trainTable > tbody").append("<tr><th>" + trainName + "</th><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivalTime + "</td><td>" + nextTrain + "</td></tr>");
     });
 
-    // // Start Clock With Current Time
+    // // // Start Clock With Current Time
     function StartClockNow() {
         clockInterval = setInterval(function () {
-            //Display clock
+            //         //Display clock
             $("#currentTime").html(moment().format("H:mm"));
 
-            //update the screen every minute
+            //         //update the screen every minute
             $("#trainEntries").empty();
             database.once("value", function (snapshot) {
                 snapshot.forEach(function (childSnapshot) {
                     var key = childSnapshot.key();
-                    var childData = childSnapshot.val();
-                    // Store everything into a variable.
+                    //                 var childData = childSnapshot.val();
+                    //                 // Store everything into a variable.
                     var trainName = childSnapshot.val().trainName;
                     var destination = childSnapshot.val().destination;
                     var arrivalTime = childSnapshot.val().arrivalTime;
                     var frequency = childSnapshot.val().frequency;
 
-                    // // Calculate mins to next train
+                    //                 // // Calculate mins to next train
                     var currentTime = moment();
                     arrivalTime = moment(arrivalTime, "HH mm");
 
@@ -115,15 +122,15 @@ $(document).ready(function () {
                         var arrivalTime = moment().add(nextTrain, "minutes").format("HH:mm");
                     }
 
-                    // Add each train's data into the table 
-                    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivalTime + "</td><td>" + nextTrain + "</td></tr>");
+                    //                 // Add each train's data into the table 
+                    $("#trainEntries > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivalTime + "</td><td>" + nextTrain + "</td></tr>");
                 });
 
             });
 
         }, 1000 * 60);
-    }
+        // }
 
-    $("#currentTime").html(moment().format("H:mm"));
-    StartClockNow()
-});
+        $("#currentTime").html(moment().format("H:mm"));
+        StartClockNow()
+    });
