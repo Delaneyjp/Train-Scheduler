878b08cf8f016ae710ae499384e2433a5eff854a
JPDent <jdelaney.ent@outlook.com>
2019-04-30 21:28:11 -0700

    fixed bug

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,10 +31,11 @@ $(document).ready(function () {
         console.log(arrivalTime);
         console.log(frequency);
 
-        $("#train-column").text(snapshot.val().trainName);
-        $("#train-destination").text(snapshot.val().destination);
-        $("#train-arrival").text(snapshot.val().arrivalTime);
-        $("#train-freq").text(snapshot.val().frequency);
+        // TESTING FUNCTION SO FAR
+        // $("#train-column").text(snapshot.val().trainName);
+        // $("#train-destination").text(snapshot.val().destination);
+        // $("#train-arrival").text(snapshot.val().arrivalTime);
+        // $("#train-freq").text(snapshot.val().frequency);
 
         // database.ref().push({
         //     trainName: trainName,
@@ -54,83 +55,53 @@ $(document).ready(function () {
         // Upload train data to Firebase
         // database.push
         database.ref().push(newTrain);
-
-
-
     });
 
 
-    // // // When a firebase event is detected, add a row for the new train
+    // When a firebase event is detected, add a row for the new train
     database.ref().on("child_added", function (childSnapshot) {
 
-        //     //     // Store everything into a variable.
-        // var trainName = childSnapshot.val().trainName;
-        // var destination = childSnapshot.val().destination;
-        // var arrivalTime = childSnapshot.val().arrivalTime;
-        // var frequency = childSnapshot.val().frequency;
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().trainName;
+        var destination = childSnapshot.val().destination;
+        var startTime = childSnapshot.val().arrivalTime;
+        var frequency = childSnapshot.val().frequency;
 
 
-        //     // Calculate mins to next train
+        // Calculate mins to next train
         var currentTime = moment();
-        arrivalTime = moment(arrivalTime, "HH mm");
+        startTime = moment(startTime, "HH mm");
 
-        if (currentTime < arrivalTime) {
-            var arrivalTime = moment(arrivalTime).format("HH:mm");
-            var nextTrain = moment.duration(arrivalTime.diff(currentTime));
-            var nextTrain = Math.round(nextTrain.asMinutes());
-        } else {
-            var nextTrain = moment.duration(currentTime.diff(arrivalTime));
-            var nextTrain = Math.round(nextTrain.asMinutes());
-            var nextTrain = frequency - (nextTrain % frequency);
-            var arrivalTime = moment().add(nextTrain, "minutes").format("HH:mm");
+
+        var minutesUntilNextTrain;
+        var nextArrival;
+        var maxMoment = moment.max(moment(), startTime);
+
+        if (maxMoment === startTime) {
+            //start time is in future
+            minutesUntilNextTrain = startTime.diff(moment(), 'minutes');
+            nextArrival = startTime;
         }
+        else {
+            //start time is in past
+            minutesUntilNextTrain = frequency - moment().diff(startTime, 'minutes') % frequency;
+            nextArrival = moment().add(minutesUntilNextTrain, 'minutes');
+        }
+        console.log(nextArrival.format('hh:mm'));
 
-        //     // Add each train's data into the table 
-        $("#trainTable > tbody").append("<tr><th>" + trainName + "</th><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivalTime + "</td><td>" + nextTrain + "</td></tr>");
+
+
+        // Add each train's data into the table 
+        $("#trainTable > tbody").append("<tr><th>" + trainName + "</th><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival.format('hh:mm A') + "</td><td>" + minutesUntilNextTrain + "</td></tr>");
     });
 
-    // // // Start Clock With Current Time
+    // Start Clock With Current Time
     function StartClockNow() {
         clockInterval = setInterval(function () {
-            //         //Display clock
-            $("#currentTime").html(moment().format("H:mm"));
+            $("#currentTime").html(moment().format("hh:mm:ss A"));
 
-            //         //update the screen every minute
-            $("#trainEntries").empty();
-            database.once("value", function (snapshot) {
-                snapshot.forEach(function (childSnapshot) {
-                    var key = childSnapshot.key();
-                    //                 var childData = childSnapshot.val();
-                    //                 // Store everything into a variable.
-                    var trainName = childSnapshot.val().trainName;
-                    var destination = childSnapshot.val().destination;
-                    var arrivalTime = childSnapshot.val().arrivalTime;
-                    var frequency = childSnapshot.val().frequency;
+        }, 1000);
+    }
 
-                    //                 // // Calculate mins to next train
-                    var currentTime = moment();
-                    arrivalTime = moment(arrivalTime, "HH mm");
-
-                    if (currentTime < arrivalTime) {
-                        var arrivalTime = moment(arrivalTime).format("HH:mm");
-                        var nextTrain = moment.duration(arrivalTime.diff(currentTime));
-                        var nextTrain = Math.round(nextTrain.asMinutes());
-                    } else {
-                        var nextTrain = moment.duration(currentTime.diff(arrivalTime));
-                        var nextTrain = Math.round(nextTrain.asMinutes());
-                        var nextTrain = frequency - (nextTrain % frequency);
-                        var arrivalTime = moment().add(nextTrain, "minutes").format("HH:mm");
-                    }
-
-                    //                 // Add each train's data into the table 
-                    $("#trainEntries > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivalTime + "</td><td>" + nextTrain + "</td></tr>");
-                });
-
-            });
-
-        }, 1000 * 60);
-        // }
-
-        $("#currentTime").html(moment().format("H:mm"));
-        StartClockNow()
-    });
+    StartClockNow()
+});
